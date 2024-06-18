49fae925ff5a1177ffd75a29f1f614f1165019a0
clarrydonovan <mbadingaclarry@gmail.com>
2024-06-18 12:38:55 +0000

    message

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,19 +3,20 @@
  * Auth:clarrydonovan
  */
 
-#include <stidlib.lib>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
- * main - is the random key allowing you to print a number and target it if it is positive or negative
- *
+ * main - is the random key allowing you to print a number
+ * and target it if it is positive or negative
  * Return: Always 0
  *
  */
 int main(void)
 {
 	int n;
+
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
@@ -23,7 +24,7 @@ int main(void)
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
-		printf("%d is zero\n"; n);
+		printf("%d is zero\n", n);
 
 	return (0);
 }

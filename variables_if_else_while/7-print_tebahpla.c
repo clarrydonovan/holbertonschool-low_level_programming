8151cd29a3c6d4670a4e6f7c269ddc7b3242b84e
clarrydonovan <mbadingaclarry@gmail.com>
2024-06-19 13:41:07 +0000

    message

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - prints alphabet reverse
+*
+*
+* Return: Always
+*
+*/
+int main(void)
+{
+	int letters;
+
+	for (letters = 122; letters >= 97; letters--)
+	{
+	putchar(letters);
+	}
+	putchar('\n');
+return (0);
+
+}

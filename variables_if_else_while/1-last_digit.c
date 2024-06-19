0657fdc7b88c6653b3e5e436a197f6f0ac49a90f
clarrydonovan <mbadingaclarry@gmail.com>
2024-06-19 08:25:58 +0000

    message

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main -allows you yo name a random number randomly to the variable n
+*
+* Return: Always
+*/
+
+int main(void)
+{
+	int n, l
+
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10
+	if (last_digit > 5)
+	{
+	printf("last digit of %d is %d and is 0\n", n, l);
+	}
+
+	else if (l == 0Â)
+	{
+	printf("last digit of %d is %d and is 0\n", n l);
+	}
+	else
+	{
+	printf("last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
+	return (0);
+}

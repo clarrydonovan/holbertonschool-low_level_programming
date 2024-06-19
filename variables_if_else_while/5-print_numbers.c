4698dbffef6b2e47a96032077ece348f37debc6d
clarrydonovan <mbadingaclarry@gmail.com>
2024-06-19 13:06:55 +0000

    message

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - prints a single numbers
+*
+* Return: Always
+*
+*/
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers < 10; numbers++)
+	{
+	printf("%d", numbers);
+	}
+putchar('n');
+return (0);
+}
+

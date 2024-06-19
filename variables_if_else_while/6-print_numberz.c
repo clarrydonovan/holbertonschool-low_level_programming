ac69fe24929f29ea9e41f6f7e37078a64ec3e25f
clarrydonovan <mbadingaclarry@gmail.com>
2024-06-19 13:51:13 +0000

    message

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - prints numbers z
+*Return: 0
+*/
+int main(void)
+{
+int numbers;
+
+for (numbers = '0'; numbers <= '9'; numbers++)
+{
+putchar(numbers);
+}
+putchar('\n');
+return (0);
+
+}

039dd5e93325d2c5b60808fc2f66b7ad10c5ec5e
clarrydonovan <mbadingaclarry@gmail.com>
2024-06-20 11:45:07 +0000

    message

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - prints base
+*
+* return Always
+*
+*/int main(void)
+{
+	char character;
+	int number;
+
+	for (number = 0; number <=  9; number++)
+		putchar(number + '0');
+	for (character = 'a'; character <= 'f'; character++)
+{
+	putchar(character);
+}
+putchar('\n');
+return (0);
+}
+

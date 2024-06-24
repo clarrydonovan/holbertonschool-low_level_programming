06514b9d03367ceafce9a368f3b0ee7347b8e4eb
clarrydonovan <mbadingaclarry@gmail.com>
2024-06-24 13:36:38 +0000

    message

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
-	/**
-	*print_alphabet - check the code
-	*
-	*Return: Always 0
-	*/
-	void print_alphabet(void)
-	{ char alphabet;
-	  for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	    {_putchar(alphabet);
-	    }
-	  _putchar('\n');
-	}
-      
-        
+/**
+*print_alphabet - check the code
+*
+*Return: Always 0
+*/
+void print_alphabet(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+_putchar('\n');
+}
